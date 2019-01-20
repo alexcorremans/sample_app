68dd80fd5891e4710ba207837b966ab2cb1a01f3
Alexandra Corremans <alexandra.corremans@gmail.com>
2019-01-20 16:21:10 +1100

    length and format validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, 
+                    length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 end
