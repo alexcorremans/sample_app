41f2435f05b66aa41636235dfa58a94e6cf7ad58
Alexandra Corremans <alexandra.corremans@gmail.com>
2019-01-20 16:55:55 +1100

    add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
                     length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password,  presence: true,
+                        length: { minimum: 6 }
 end
